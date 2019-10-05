94114bf7b11af96214b5aa23088f72a8cd8e15b4
felix <felixmariotto@gmail.com>
2019-10-05 23:15:31 +0800

    initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.static('public'));
+
+app
+    .get('/', (req, res)=> {
+        res.sendFile(path.join(__dirname + '/public/index.html'));
+    })
+
+    .listen(PORT, ()=> {
+        console.log('App listening on port ' + PORT);
+    })
