dd5953fe1889547f191bf231feadea83195a9fe3
felix <felixmariotto@gmail.com>
2019-10-24 22:06:03 +0800

    improve the dialogue UI

--- public/js/Interaction.js
+++ public/js/Interaction.js
@@ -6,8 +6,11 @@ function Interaction() {
 	const domTalkContainer = document.getElementById('talk-container');
 	var isTalkContainerVisible = false ;
 
-	const domRightChar = document.getElementById('right-char');
-	const domLeftChar = document.getElementById('left-char');
+	const domCharName = document.getElementById('talker-name-container');
+
+	const domChar = document.getElementById('char-container');
+
+	const domOverlay = document.getElementById('overlay');
 
 
 	function interactWith( agentName ) {
@@ -15,34 +18,38 @@ function Interaction() {
 		// HIDE THE TALK UI
 		if ( isTalkContainerVisible ) {
 
-			domRightChar.classList.remove( 'show-right-char' );
-			domRightChar.classList.add( 'hide-right-char' );
-
-			domLeftChar.classList.remove( 'show-left-char' );
-			domLeftChar.classList.add( 'hide-left-char' );
+			domChar.classList.remove( 'show-char' );
+			domChar.classList.add( 'hide-char' );
 
 			// Hide the talk container after hiding the characters
 			setTimeout( ()=> {
 
+				domOverlay.style.display = 'none' ;
+
 				domTalkContainer.classList.remove( 'show-talk' );
 				domTalkContainer.classList.add( 'hide-talk' );
+
+				domCharName.classList.remove( 'show-talker-name' );
+				domCharName.classList.add( 'hide-talker-name' );
 
 			}, 300);
 
 		// SHOW THE TALK UI
 		} else {
 
+			domOverlay.style.display = 'inherit' ;
+
 			domTalkContainer.classList.remove( 'hide-talk' );
 			domTalkContainer.classList.add( 'show-talk' );
+
+			domCharName.classList.remove( 'hide-talker-name' );
+			domCharName.classList.add( 'show-talker-name' );
 
 			// show the characters after showing the talk container
 			setTimeout( ()=> {
 
-				domRightChar.classList.remove( 'hide-right-char' );
-				domRightChar.classList.add( 'show-right-char' );
-
-				domLeftChar.classList.remove( 'hide-left-char' );
-				domLeftChar.classList.add( 'show-left-char' );
+				domChar.classList.remove( 'hide-char' );
+				domChar.classList.add( 'show-char' );
 
 			}, 200);
 
