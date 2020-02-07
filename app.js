338d32f926050ed6a4b07f2f91d4e6fcb0ee03f0
Felix Mariotto <felixmariotto@gmail.com>
2020-02-07 14:48:03 +0800

    add test postgres query to /db

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 5050;
 
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+});
+
 
 app.use(express.static('public'));
 
@@ -11,6 +17,23 @@ app
     .get('/', (req, res)=> {
         res.sendFile(path.join(__dirname + '/public/index.html'));
     })
+
+
+
+    .get('/db', async (req, res) => {
+	    try {
+	      const client = await pool.connect()
+	      const result = await client.query('SELECT * FROM test_table');
+	      const results = { 'results': (result) ? result.rows : null};
+	      res.render('pages/db', results );
+	      client.release();
+	    } catch (err) {
+	      console.error(err);
+	      res.send("Error " + err);
+	    }
+	  })
+
+
 
     .listen(PORT, ()=> {
         console.log('App listening on port ' + PORT);
