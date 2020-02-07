29ca9982b1e5bf1355bfa0bce5301f58b8d07859
Felix Mariotto <felixmariotto@gmail.com>
2020-02-07 15:20:11 +0800

    update table name

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app
     .get('/db', async (req, res) => {
 	    try {
 	      const client = await pool.connect()
-	      const result = await client.query('SELECT * FROM test_table');
+	      const result = await client.query('SELECT * FROM analytics');
 	      const results = { 'results': (result) ? result.rows : null};
 	      res.render('pages/db', results );
 	      client.release();
