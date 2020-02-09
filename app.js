e67ab591340dd531fc15cc2f143ffe8af67b4ec3
felix <felixmariotto@gmail.com>
2020-02-09 14:43:25 +0800

    fix sql array

--- app.js
+++ app.js
@@ -113,7 +113,7 @@ io.on( 'connection', async (client)=> {
 		var postgresClient = await POOL.connect();
 
 		postgresClient.query( `UPDATE analytics SET
-								deaths = deaths || '{${ message.timestamp }, ${ message.position }}'
+								deaths = deaths || '{"${ message.timestamp }", "${ message.position }"}'
 							   WHERE id = ${ clientID }` );
 
 		postgresClient.release();
@@ -129,7 +129,7 @@ io.on( 'connection', async (client)=> {
 		var postgresClient = await POOL.connect();
 
 		postgresClient.query( `UPDATE analytics SET
-								opti_levels = opti_levels || '{${ message.timestamp }, ${ message.level }}'
+								opti_levels = opti_levels || '{"${ message.timestamp }", "${ message.level }"}'
 							   WHERE id = ${ clientID }` );
 
 		postgresClient.release();
