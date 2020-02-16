ad1f3a08607c21dd64e9cffb73926bbde7eaf5d3
felix <felixmariotto@gmail.com>
2020-02-16 19:45:18 +0800

    add positionalAudio elements and their parameters

--- public/js/soundMixer.js
+++ public/js/soundMixer.js
@@ -31,9 +31,64 @@ function SoundMixer() {
 
 	const SFX_PARAMS = {
 		faint_waves: {
+			volume: 0.8,
+			distance: 1,
+			maxDistance: 12
+		},
+		cricket: {
+			volume: 0.5,
+			distance: 1,
+			maxDistance: 15,
+			playSpeedVarying: 0.1
+		},
+		cricket2: {
+			volume: 0.55,
+			distance: 1.1,
+			maxDistance: 15,
+			playSpeedVarying: 0.05
+		},
+		fly: {
+			volume: 1,
+			distance: 1,
+			maxDistance: 4,
+			playSpeedVarying: 0.1
+		},
+		swallow: {
+			volume: 0.45,
+			distance: 1,
+			maxDistance: 14
+		},
+		robin: {
 			volume: 0.7,
-			distance: 3,
-			maxDistance: 10
+			distance: 1.3,
+			maxDistance: 16
+		},
+		nutcracker: {
+			volume: 0.6,
+			distance: 1.3,
+			maxDistance: 16,
+			playSpeedVarying: 0.1
+		},
+		stream: {
+			volume: 1,
+			distance: 0.5,
+			maxDistance: 13,
+			playSpeedVarying: 0.1
+		},
+		waterfall: {
+			volume: 1.4,
+			distance: 0.4,
+			maxDistance: 25
+		},
+		small_waterfall: {
+			volume: 1.3,
+			distance: 0.5,
+			maxDistance: 15
+		},
+		gust: {
+			volume: 0.9,
+			distance: 1,
+			maxDistance: 20
 		}
 	}
 
@@ -61,7 +116,7 @@ function SoundMixer() {
 
     function start() {
 
-    	setMusicSrc( 0 );
+    	// setMusicSrc( 1 );
 
     	listener = new THREE.AudioListener();
 		camera.add( listener );
@@ -104,17 +159,27 @@ function SoundMixer() {
 			sound.setRefDistance( SFX_PARAMS[ sfxName ].distance );
 			sound.setVolume( SFX_PARAMS[ sfxName ].volume );
 			sound.maxDistance = SFX_PARAMS[ sfxName ].maxDistance
-
 			sound.setLoop( true );
-			sound.play();
-			
+			sound.sfxName = sfxName ;
+
+			if ( SFX_PARAMS[ sfxName ].playSpeedVarying ) {
+				sound.setPlaybackRate(
+					( 1 - SFX_PARAMS[ sfxName ].playSpeedVarying ) -
+					( Math.random() * SFX_PARAMS[ sfxName ].playSpeedVarying )
+				);
+			};
+
+			sound.autoplay = true;
+
+			sound.position.copy( position );
+
+			scene.add( sound );
+
+			sfxs.push( sound );
+
 		});
 
-		sound.position.copy( position );
-
-		scene.add( sound );
-
-		sfxs.push( sound );
+		
 
     };
 
@@ -140,9 +205,13 @@ function SoundMixer() {
 
 			sfxCanPlay = false ;
 
+			console.log( sfxs );
+
 			for ( let sound of sfxs ) {
 
-				sound.stop();
+				console.log( sound )
+
+				if ( sound.isPlaying ) sound.stop();
 
 			};
 
@@ -165,7 +234,13 @@ function SoundMixer() {
 
 				} else {
 
-					if ( !sound.isPlaying ) sound.play();
+					if ( !sound.isPlaying ) {
+
+						sound.play();
+
+						console.log( sound.sfxName );
+
+					};
 
 				};
 
